a7713db14c88645fcc44b1eb5cde9e2485c2e06b
kesquivel <kesquivel@senacyt.gob.gt>
2019-10-22 16:32:48 -0600

    Avances al 22/10/2019

--- ECYS-Lab-Captive-Portal/src/java/servletsModel/Usuario.java
+++ ECYS-Lab-Captive-Portal/src/java/servletsModel/Usuario.java
@@ -9,11 +9,6 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import org.json.JSONException;
-import org.json.JSONObject;
 
 /**
  *
